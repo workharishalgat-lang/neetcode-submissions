aa0ed1865de732e5654079b58eca97f5330d2819
workharishalgat-lang <work.harishalgat@gmail.com>
2026-08-04 21:59:27 +0530

    Update: two-integer-sum - submission-1

--- Data Structures & Algorithms/two-integer-sum/submission-1.go
+++ Data Structures & Algorithms/two-integer-sum/submission-1.go
@@ -1,13 +1,15 @@
 func twoSum(nums []int, target int) []int {
-    visited :=  make(map[int]int)
-	visited[nums[0]] = 0 
-    for i:=1;i<len(nums);i++ {
-		our :=  target - nums[i]
-		if v,ok := visited[our]; ok {
-			return []int{v,i}
-		}else{
-			visited[nums[i]] = i
-		}
-	}
-	return []int{0,0}
+	data := map[int]int{}
+    
+   data[nums[0]] = 0 
+   for i:=1;i<len(nums);i++ {
+	  v := target - nums[i]
+	  if j, present := data[v];  present {
+		return []int{j,i}
+	  }else{
+		data[nums[i]] = i
+	  } 
+   }
+   return []int{0,0}
+    
 }
